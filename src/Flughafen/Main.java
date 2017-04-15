7656d7e2c9f0fb32b968d6217f45e6a2bcdb2e83
GagaLP <GabrielMitterrutzner@gmail.com>
2017-04-15 18:23:19 +0200

    Working Gui Branche

--- src/Flughafen/Main.java
+++ src/Flughafen/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("./Flughafen/Views/StartScene.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("Views/StartScene.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
