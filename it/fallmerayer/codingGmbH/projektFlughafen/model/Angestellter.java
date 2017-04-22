b2e5d128b0de05d0b0d3ca9c7f1ef7103ab005b5
hannesNiederwolfsgruber <hannes.nwg@gmail.com>
2017-04-22 16:42:26 +0200

    Update Angestellter.java

--- it/fallmerayer/codingGmbH/projektFlughafen/model/Angestellter.java
+++ it/fallmerayer/codingGmbH/projektFlughafen/model/Angestellter.java
@@ -1,6 +1,7 @@
 package it.fallmerayer.codingGmbH.projektFlughafen.model;
 import java.time.LocalDateTime;
 import java.util.List;
+
 /**
  * Created by Hannes Niederwolfsgruber on 11.04.2017.
  */
@@ -11,13 +12,15 @@ public class Angestellter extends Benutzerprofil {
 
     public Angestellter(String vorname, String nachname, String email, LocalDateTime geburtsDatum, String benutzerName, String passwort) throws TooMuchAngestellteException {
         super(vorname, nachname, email, geburtsDatum, benutzerName, passwort);
-        if ((angestellterCounter + 1) <= ANGESTELLTER_MAX){
-            this.setPID(angestellterCounter++);
-        }else {
-            throw new TooMuchAngestellteException();
+        if(!(this instanceof Administrator)){
+            if ((angestellterCounter + 1) <= ANGESTELLTER_MAX){
+                this.setPID(angestellterCounter++);
+            }else {
+                throw new TooMuchAngestellteException();
+            }
         }
     }
-
+    
     public static int getAngestellterCounter() {
         return angestellterCounter;
     }
