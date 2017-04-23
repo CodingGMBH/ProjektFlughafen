baba949b9be0f3e1315df1cf26c6f6bdf1958ed5
hannesNiederwolfsgruber <hannes.nwg@gmail.com>
2017-04-23 15:18:21 +0200

    Update BuchungsprofilAngestellter.java

--- it/fallmerayer/codingGmbH/projektFlughafen/model/BuchungsprofilAngestellter.java
+++ it/fallmerayer/codingGmbH/projektFlughafen/model/BuchungsprofilAngestellter.java
@@ -16,7 +16,7 @@ public class BuchungsprofilAngestellter extends Buchungsprofil {
     public BuchungsprofilAngestellter(String flugNummer, double gepaeckGewicht, int angestellterPID, List<Mitflieger> passagierListe) throws FlugNichtBuchbarException {
         super(flugNummer, gepaeckGewicht);
         this.angestellterPID = angestellterPID;
-        if ((FluegeSpeicher.getInstance.getFlug(this.getFlugNummer()).getFlugzeug().getAnzahlSitzplaetze() - FluegeSpeicher.getInstance().getFlug(this.getFlugNummer()).getGebuchteSitzplaetze()) >= passagierListe.size()){
+        if ((FluegeSpeicher.getInstance().getFlug(this.getFlugNummer()).getFlugzeug().getAnzahlSitzplaetze() - FluegeSpeicher.getInstance().getFlug(this.getFlugNummer()).getGebuchteSitzplaetze()) >= passagierListe.size()){
             FluegeSpeicher.getInstance().getFlug(this.getFlugNummer()).setZaehlerGebuchteSitzplaetze(FluegeSpeicher.getInstance().getFlug(this.getFlugNummer()).getZaehlerGebuchteSitzplaetze() + passagierListe.size());
             this.passagierListe = passagierListe;
         }else {
