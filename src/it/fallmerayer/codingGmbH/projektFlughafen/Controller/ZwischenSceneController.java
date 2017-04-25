b717cf399e7e92e64542ed441e53b103334a3bcc
GagaLP <GabrielMitterrutzner@gmail.com>
2017-04-26 00:01:34 +0200

    Zusemmenfügen von Controller und Modell 1/2

--- src/it/fallmerayer/codingGmbH/projektFlughafen/Controller/ZwischenSceneController.java
+++ src/it/fallmerayer/codingGmbH/projektFlughafen/Controller/ZwischenSceneController.java
@@ -24,6 +24,7 @@ public class ZwischenSceneController extends AbstractController {
             erfolgreichLabel.setText("Zahlung erfolgreich");
         } else if (main.lastController.matches(ViewNavigation.ANMELDENREGISTRIRENSCENE)){
             erfolgreichLabel.setText("Erfolgreich angemeldet/registriert");
+
         }
     }
 
